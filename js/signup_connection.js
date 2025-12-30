3755e2914df4450a494b88223c9127e5a55ff52f
aswinfreshworks1309-max <aswinfreshworks1309@gmail.com>
2025-12-30 22:16:10 +0530

    api changed

--- js/signup_connection.js
+++ js/signup_connection.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       try {
-        const response = await fetch("http://project-backend-h7p0ce6q5-aswins-projects-7ca69fa9.vercel.app/api/users/", {
+        const response = await fetch("http://127.0.0.1:8000/api/users/", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
