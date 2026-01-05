f699579f3c6b1c1ed0055990655e799ead726f87
aswinfreshworks1309-max <aswinfreshworks1309@gmail.com>
2026-01-05 17:11:46 +0530

    api changed

--- js/signup_connection.js
+++ js/signup_connection.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
 
       try {
-        const response = await fetch(`${API_BASE_URL}/api/users/`, {
+        const response = await fetch(`${API_BASE_URL}/users/`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
