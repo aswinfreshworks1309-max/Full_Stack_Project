71c19022d3124958a4aff7d7fc5d0e413aa35f6c
aswinfreshworks1309-max <aswinfreshworks1309@gmail.com>
2026-01-06 09:49:58 +0530

    api changed

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Configuration for the API
-const API_BASE_URL = "locotranz-git-main-aswins-projects-7ca69fa9.vercel.app";
+const API_BASE_URL = "https://project-backend-rose-nine.vercel.app";
 
 // You can easily switch to local for development
 // const API_BASE_URL = "http://127.0.0.1:8000";
