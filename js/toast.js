e191cd91ccc342862e13a2472b801bfa53d85ce5
aswinfreshworks1309-max <aswinfreshworks1309@gmail.com>
2026-01-29 01:38:56 +0530

    unwanted function removed

--- js/toast.js
+++ js/toast.js
@@ -20,16 +20,16 @@ function showToast(message, type = "success") {
   // Add to container
   container.appendChild(toast);
 
-  // Auto remove after 4 seconds
-  setTimeout(() => {
-    toast.classList.add("hiding");
+    // Auto remove after 4 seconds
     setTimeout(() => {
-      if (toast.parentNode === container) {
-        container.removeChild(toast);
-      }
-    }, 300);
-  }, 4000);
-}
+      toast.classList.add("hiding");
+      setTimeout(() => {
+        if (toast.parentNode === container) {
+          container.removeChild(toast);
+        }
+      }, 300);
+    }, 4000);
+  }
 
 // Optional: Override window.alert if needed, but it's better to explicitly call showToast
 // window.alert = (msg) => showToast(msg, 'info');
