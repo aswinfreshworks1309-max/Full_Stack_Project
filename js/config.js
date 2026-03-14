0d21165004a37ab7be8a76a9faa2f9b959766933
aswinfreshworks1309-max <aswinfreshworks1309@gmail.com>
2026-03-14 17:01:56 +0530

    backend url changed

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Configuration for the API
 // const API_BASE_URL = "https://project-backend-fcwe.onrender.com";  RENDER
-const API_BASE_URL =  " https://project-backend-kmikz22c2-aswins-projects-7ca69fa9.vercel.app";
+const API_BASE_URL = "https://project-backend-tau-sable.vercel.app/";
 
 // You can easily switch to local for development
 // const API_BASE_URL = "http://127.0.0.1:8000";
